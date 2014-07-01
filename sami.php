f8d61fb456b2fe348841de3de7096150149c1d78
Steffen Gransow <graste@mivesto.de>
2014-07-01 11:55:29 +0200

    change sami cfg to only create docs of current wc

--- sami.php
+++ sami.php
@@ -6,11 +6,11 @@ use Symfony\Component\Finder\Finder;
 
 $iterator = Finder::create()->files()->name('*.php')->in(__DIR__ . DIRECTORY_SEPARATOR . 'src');
 
-$versions = GitVersionCollection::create(__DIR__)->addFromTags('*')->add('master', 'master branch');
+//$versions = GitVersionCollection::create(__DIR__)->add('master', 'master branch')->addFromTags('*');
 
 return new Sami($iterator, array(
     'theme'                => 'github',
-    'versions'             => $versions,
+//    'versions'             => $versions,
     'title'                => 'Parameters API',
     'build_dir'            => __DIR__ . str_replace('/', DIRECTORY_SEPARATOR, '/docs/api/%version%'),
     'cache_dir'            => __DIR__ . str_replace('/', DIRECTORY_SEPARATOR, '/build/cache/%version%'),
